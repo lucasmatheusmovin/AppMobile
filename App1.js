b82890560ae3677f79f453b2799d38eeaa20f1cf
Zerofake <51544336+Zerofake@users.noreply.github.com>
2021-05-05 16:47:17 -0300

    Pedido 1
    Tela de Pedido sendo montada

---
+++ App1.js
@@ -0,0 +1,14 @@
+import React from 'reat';
+import {View} from 'react-native';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+import { Txt,  Button, TextInput} from 'react-native';
+
+const Stack = creatStackNavigator();
+
+const INITIAL_STATE = {
+
+ 
+}
